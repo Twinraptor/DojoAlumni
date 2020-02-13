19994cc1a41c8491ad8c4d4d2e157c0d545a3535
twinraptor <mythosx@hotmail.com>
2020-02-13 01:42:05 -0800

    Added the login actions, types, and reducers

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
 
-export const Login = () => {
+export const Login = ({ login }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
@@ -14,7 +17,8 @@ export const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log("Success");
+    console.log("this should send email and password", email, password);
+    login(email, password);
   };
 
   return (
@@ -53,4 +57,8 @@ export const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(Login);

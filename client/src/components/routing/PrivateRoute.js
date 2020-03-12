063e96c5a0ee41b408fa5ef6aff7150f8453a71a
twinraptor <mythosx@hotmail.com>
2020-03-12 08:59:11 -0700

    fixed missing single bracket and div close.

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -7,8 +7,18 @@ const PrivateRoute = ({
   component: Component,
   auth: { isAuthenticated, loading },
   ...rest
-}) => (<Route {...rest} render={props => !isAuthenticated && !loading ? (<Redirect to='/login' />) : ( <Component {...props} /> )
-)
+}) => (
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated && !loading ? (
+        <Redirect to='/login' />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+);
 PrivateRoute.propTypes = {
   auth: PropTypes.object.isRequired
 };
